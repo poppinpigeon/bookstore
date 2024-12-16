bf5e960a1ec5325ccab544be6c9f7305787e09f6
Jade Kim <csia19030@gmail.com>
2024-12-16 23:42:34 +0900

    feat: implement api for add like and remove like

--- routes/likes.js
+++ routes/likes.js
@@ -1,16 +1,14 @@
+const {addLike, removeLike} = require('../controller/likeController');
+
 const express = require('express');
 const router = express.Router();
 
 router.use(express.json());
 
 //add like
-router.post('/:bookId', (req, res) => {
-    res.json('add like');
-});
+router.post('/:book_id', addLike);
 
 //remove like
-router.delete('/:bookId', (req, res) => {
-    res.json('remove like');
-});
+router.delete('/:book_id', removeLike);
 
 module.exports = router;
